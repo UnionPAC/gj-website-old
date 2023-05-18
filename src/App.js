8f366fa7eeb1479bfeb70d42573ec0ff4aeb1a84
Geoff <geoff.ijamieson@gmail.com>
2023-05-18 09:29:36 -0400

    changed favicon and added mailto link

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import { Navbar } from "./components/Navbar";
 import { Showcase } from "./components/Showcase";
 import { About } from "./components/About";
 import { Portfolio } from "./components/Portfolio";
-/* import { Blog } from "./components/Blog"; */
 import { Interests } from "./components/Interests";
 import { Footer } from "./components/Footer";
 
@@ -13,7 +12,6 @@ function App() {
       <Showcase />
       <About />
       <Portfolio />
-      {/* <Blog /> */}
       <Interests />
       <Footer />
     </div>
--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,25 +4,38 @@ export const Footer = () => {
   return (
     <div
       id="contact"
-      className="flex flex-col md:flex-row-reverse md:justify-around  justify-center items-center bg-[#030303] py-4 text-white pb-8 md:pb-4"
+      className="flex flex-col md:flex-row-reverse md:justify-around  justify-center items-center  pb-8"
     >
       <ul className="text-3xl pt-4 pb-10 md:pb-6">
-        <a href="https://github.com/UnionPAC" target='_blank' rel="noreferrer">
+        <a href="https://github.com/UnionPAC" target="_blank" rel="noreferrer">
           <i className="fa-brands fa-github px-2 md:mx-6 hover:scale-110" />
         </a>
-        <a href="https://medium.com/@geoff_jamieson" target='_blank' rel="noreferrer">
-          <i className="fa-brands fa-medium px-2 md:px-6 hover:scale-110" />
-        </a>
-        <a href="https://www.linkedin.com/in/geoff-jamieson-4b1b94255/" target='_blank' rel="noreferrer">
+
+        <a
+          href="https://www.linkedin.com/in/geoff-jamieson-4b1b94255/"
+          target="_blank"
+          rel="noreferrer"
+        >
           <i className="fa-brands fa-linkedin px-2 md:px-6 hover:scale-110" />
         </a>
-        <a href="https://twitter.com/unionpac_" target='_blank' rel="noreferrer">
+        <a
+          href="https://twitter.com/unionpac_"
+          target="_blank"
+          rel="noreferrer"
+        >
           <i className="fa-brands fa-twitter px-2 md:px-6 hover:scale-110" />
+        </a>
+        <a
+          href="mailto:geoff.ijamieson@gmail.com"
+          target="_blank"
+          rel="noreferrer"
+        >
+          <i className="fa-solid fa-paper-plane px-2 md:px-6 hover:scale-110" />
         </a>
       </ul>
       <div className="flex justify-center items-center">
         <p className="text-[12px] lg:text-[15px] mb-2 md:mb-0 tracking-wide">
-          Website built by Geoff Jamieson
+          🌸 Designed and Developed by Geoff Jamieson
         </p>
       </div>
     </div>
